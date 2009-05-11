89ccc8307f9f820b06b7c774273ccf9bef749ad7
monsieurvideo <monsieurvideo@021c3042-21ff-11de-9909-f37ff7dbbc12>
2009-05-11 17:17:08 +0000

    Version 1.10

---
+++ tags/v1.10/FlashVideo/RTMPDownloader.pm
@@ -0,0 +1,106 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::RTMPDownloader;
+
+use strict;
+use base 'FlashVideo::Downloader';
+use Fcntl;
+use IPC::Open3;
+use Symbol qw(gensym);
+use FlashVideo::Utils;
+
+sub download {
+  my ($self, $rtmp_data) = @_;
+
+  if (-e $rtmp_data->{flv} && !$rtmp_data->{live}) {
+    info "RTMP output filename '$rtmp_data->{flv}' already " .
+                 "exists, asking rtmpdump to resume...";
+    $rtmp_data->{resume} = '';
+  }
+
+  $self->{filename} = $rtmp_data->{flv};
+
+  my($r_fh, $w_fh); # So Perl doesn't close them behind our back..
+
+  if ($rtmp_data->{live} && $::opt{play}) {
+    # Playing live stream, we pipe this straight to the player, rather than
+    # saving on disk.
+    pipe($r_fh, $w_fh);
+
+    my $pid = fork;
+    die "Fork failed" unless defined $pid;
+    if(!$pid) {
+      fcntl $r_fh, F_SETFD, ~FD_CLOEXEC;
+      exec $self->replace_filename($::opt{player}, "/dev/fd/" . fileno $r_fh);
+      die "Exec failed\n";
+    }
+
+    fcntl $w_fh, F_SETFD, ~FD_CLOEXEC;
+    $rtmp_data->{flv} = "/dev/fd/" . fileno $w_fh;
+
+    $self->{stream} = undef;
+  }
+
+  debug "Running rtmpdump", 
+    join(" ", map { ("--$_" => $rtmp_data->{$_} ? $self->shell_escape($rtmp_data->{$_}) : ()) } keys
+        %$rtmp_data);
+
+  my($in, $out, $err);
+  $err = gensym;
+  open3($in, $out, $err, "rtmpdump",
+    map { ("--$_" => ($rtmp_data->{$_} || ())) } keys %$rtmp_data);
+
+  my $buf = "";
+  while(sysread($err, $buf, 512, length $buf) > 0) {
+    my @parts = split /\r/, $buf;
+    $buf = "";
+
+    for(@parts) {
+      # Hide almost everything from rtmpdump, it's less confusing this way.
+      if(/^((?:DEBUG:|WARNING:|Closing connection).*)\n/) {
+        debug "rtmpdump: $1";
+      } elsif(/^(ERROR: .*)\n/) {
+        info "rtmpdump: $1";
+      } elsif(/^([0-9.]+) KB(?: \(([0-9.]+)%\))?/) {
+        $self->{downloaded} = $1 * 1024;
+        my $percent = $2;
+
+        if($self->{downloaded} && $percent != 0) {
+          # An approximation, but should be reasonable if we don't have the size.
+          $self->{content_length} = $self->{downloaded} / ($percent / 100);
+        }
+
+        $self->progress;
+      } elsif(/\n$/) {
+        for my $l(split /\n/) {
+          info $l if /\w/;
+        }
+      } else {
+        # Hack; assume lack of newline means it was an incomplete read..
+        $buf = $_;
+      }
+    }
+
+    # Should be about enough..
+    if(defined $self->{stream} && $self->{downloaded} > 300_000) {
+      $self->{stream}->();
+    }
+  }
+
+  if(!$self->check_file($rtmp_data->{flv})) {
+    error "Download failed, no valid file downloaded";
+    unlink $rtmp_data->{flv};
+    return 0;
+  }
+
+  if($self->{percent} > 95) {
+    # Note we hide the progress, our download guess was only an estimate, so this
+    # should be less confusing..
+    info "\rDone. Saved", -s $self->{filename}, "bytes to $self->{filename}.";
+  } else {
+    info "\nrtmpdump exited early? Incomplete download possible -- try running again to resume.";
+  }
+
+  return 1;
+}
+
+1;
---
+++ tags/v1.10/FlashVideo/URLFinder.pm
@@ -0,0 +1,90 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::URLFinder;
+
+use strict;
+use FlashVideo::Mechanize;
+use FlashVideo::Generic;
+use URI;
+
+# The main issue is getting a URL for the actual video, so we handle this
+# here - a different package for each site, as well as a generic fallback.
+# Each package has a find_video method, which should return a URL, and a
+# suggested filename.
+
+# In some cases there isn't an obvious URL to find, so the following will be loaded and their 'can_handle'
+# method called.
+my @extra_can_handle = qw(Brightcove Mtvnservices);
+
+sub find_package {
+  my($url, $browser) = @_;
+  my $package = find_package_url($url, $browser);
+
+  if(!defined $package) {
+    # Fairly lame heuristic, look for the first URL outside the <object>
+    # element (avoids grabbing things like codebase attribute).
+    # Also look at embedded scripts for sites which embed their content that way.
+ 
+    URLS: for my $possible_url(
+        $browser->content =~ m!(?:<object[^>]+>.*?|<(?:script|embed) [^>]*src=["']?)(http://[^"'> ]+)!gx) {
+      $package = find_package_url($possible_url, $browser);
+      return $package, $possible_url if defined $package;
+    }
+  }
+
+  if(!defined $package) {
+    for(@extra_can_handle) {
+      my $possible_package = "FlashVideo::Site::$_";
+      eval "require $possible_package";
+
+      my $r = $possible_package->can_handle($browser, $url);
+      if($r) {
+        $package = $possible_package;
+        last;
+      }
+    }
+  }
+
+  if(!defined $package) {
+    $package = "FlashVideo::Generic";
+  }
+
+  return $package, $url;
+}
+
+# Split the URLs into parts and see if we have a package with this name.
+
+sub find_package_url {
+  my($url, $browser) = @_;
+  my $package;
+
+  foreach my $host_part (split /\./, URI->new($url)->host) {
+    $host_part = ucfirst lc $host_part;
+    $host_part =~ s/[^a-z0-9]//i;
+
+    my $possible_package = "FlashVideo::Site::$host_part";
+    eval "require $possible_package";
+
+    if(UNIVERSAL::can($possible_package, "find_video")) {
+
+      if($possible_package->can("can_handle")) {
+        next unless $possible_package->can_handle($browser, $url);
+      }
+
+      $package = $possible_package;
+      last;
+    }
+  }
+
+  return $package;
+}
+
+# Utility functions
+
+sub get_browser {
+  my $browser = FlashVideo::Mechanize->new(autocheck => 0);
+  $browser->agent_alias("Windows Mozilla");
+
+  return $browser;
+}
+
+1;
