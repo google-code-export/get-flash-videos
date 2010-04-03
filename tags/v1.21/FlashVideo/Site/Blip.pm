1257def3883ea5c5546dbfa1e86652d10896da5c
monsieurvideo <monsieurvideo@021c3042-21ff-11de-9909-f37ff7dbbc12>
2010-04-03 12:11:59 +0000

    Version 1.21

---
+++ tags/v1.21/FlashVideo/Site/Blip.pm
@@ -0,0 +1,53 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Site::Blip;
+
+use strict;
+use FlashVideo::Utils;
+
+sub find_video {
+  my ($self, $browser, $embed_url) = @_;
+  my $base = "http://blip.tv";
+
+  my $has_xml_simple = eval { require XML::Simple };
+  if(!$has_xml_simple) {
+    die "Must have XML::Simple installed to download Blip videos";
+  }
+
+  my $id;
+  if($embed_url =~ m{flash/(\d+)}) {
+    $id = $1;
+  } else {
+    $browser->get($embed_url);
+
+    if($browser->response->is_redirect
+        && $browser->response->header("Location") =~ m!(?:/|%2f)(\d+)!i) {
+      $id = $1;
+    } else {
+      $id = ($browser->content =~ m!/rss/flash/(\d+)!)[0];
+    }
+  }
+  die "No ID found\n" unless $id;
+
+  $browser->get("$base/rss/flash/$id");
+
+  my $xml = eval {
+    XML::Simple::XMLin($browser->content)
+  };
+
+  if ($@) {
+    die "Couldn't parse Blip XML : $@";
+  }
+
+  my $content = $xml->{channel}->{item}->{"media:group"}->{"media:content"};
+
+  my $url = ref $content eq 'ARRAY' ? $content->[0]->{url} : $content->{url};
+
+  my $filename = title_to_filename($xml->{channel}->{item}->{title}, $url);
+
+  # I want to follow redirects now.
+  $browser->allow_redirects;
+
+  return $url, $filename;
+}
+
+1;
---
+++ tags/v1.21/t/google_video_search.t
@@ -0,0 +1,22 @@
+#!perl
+use strict;
+use lib qw(..);
+use Test::More tests => 2;
+use FlashVideo::Site::Googlevideosearch;
+
+my @results = FlashVideo::Site::Googlevideosearch->search('Iron Man trailer');
+
+ok(@results > 1, "Results returned");
+
+# Check to see if the results look sane
+my $sane_result_count = 0;
+
+foreach my $result (@results) {
+  if ((ref($result) eq 'HASH') and
+      $result->{name} and
+      $result->{url} =~ m'^http://') {
+    $sane_result_count++;
+  }
+}
+
+ok($sane_result_count == @results, "Results look sane");
